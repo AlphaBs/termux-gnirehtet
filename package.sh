7d163ebf4e18504f0f9312a529aeca2c10922ab8
권세인 <ksi123456ab@naver.com>
2023-03-26 04:14:52 +0000

    add package.sh script

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+tar -czvf termux-gnirehtet.tar.gz \
+    ./adb-samsung \
+    ./build-gnirehtet \
+    ./install \
+    ./LICENSE \
+    ./README.md \
+    ./gnirehtet \
+    ./gnirehtet.apk
