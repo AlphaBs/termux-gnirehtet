75ad6ef5fd3fcc137b91a0ac6fdc8bc5d1afa272
AlphaBs <ksi123456ab@naver.com>
2023-03-29 19:57:19 +0900

    fix filenames

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 tar -czvf termux-gnirehtet.tar.gz \
-    ./adb-samsung \
-    ./build-gnirehtet \
-    ./install \
+    ./adb-samsung.sh \
+    ./build-gnirehtet.sh \
+    ./install.sh \
     ./LICENSE \
     ./README.md \
     ./gnirehtet \
