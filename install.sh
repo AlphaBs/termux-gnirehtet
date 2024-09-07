d803d6c118095c736ce91f424d635c62b5feb881
권세인 <ksi123456ab@naver.com>
2024-09-07 17:06:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 confirm() {
     # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure? [y/N]} " response
+    read -r -p "${1:-Are you sure?} [y/N]" response
     case "$response" in
         [yY][eE][sS]|[yY]) 
             true
@@ -31,18 +31,22 @@ if [ ! -f "$TERMUX_ADB" ]; then
 fi
 
 if confirm "Is your device running Samsung's Android?"; then
-    if confirm "Replace 'adb' with 'termux-adb'? (recommended)"; then
+    if confirm "Replace 'adb' with 'termux-adb'? (RECOMMENDED)"; then
         cp ./adb-samsung.sh $PREFIX/bin/adb;
     else
         echo "Set up ADB before each use of gnirehtet:"
+        echo "";
         echo "ADB=adb-samsung.sh ./gnirehtet run";
+        echo "";
     fi
 else
     if confirm "Create a symbolic link for 'adb' command?"; then
         ln -s "$PREFIX/bin/termux-adb" "$PREFIX/bin/adb";
     else
         echo "Set up ADB before each use of gnirehtet:";
+        echo "";
         echo "ADB=termux-adb ./gnirehtet run";
+        echo "";
     fi
 fi
 
